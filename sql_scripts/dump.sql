e4f3605f3638ec2a24e258bcd724fe16bb446bca
RollingWorld <stig1467@gmail.com>
2020-04-28 01:12:25 +0300

    dump edit

--- sql_scripts/dump.sql
+++ sql_scripts/dump.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `learninggroupsdb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `learninggroupsdb`;
 -- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
 --
 -- Host: localhost    Database: learninggroupsdb
@@ -358,4 +360,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-04-28  0:51:12
+-- Dump completed on 2020-04-28  1:11:30
