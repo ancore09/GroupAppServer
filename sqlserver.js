f232e4865396f84cfb20479d28a961676d5170f7
RollingWorld <stig1467@gmail.com>
2020-05-07 21:46:16 +0300

    /

--- sqlserver.js
+++ sqlserver.js
@@ -558,7 +558,7 @@ io.on('connection', function(socket) {
 		console.log(obj);
 		socket.leave(obj.prev_id);
 		socket.join(obj.new_id);
-		socket.set('nick', obj.nick)
+		socket.username(obj.nick)
 	});
 });
 
